7afa0b32d2bc4212c7762f2c8209f9c997cd33c6
syahfrinorezky <syahfrinorezky@gmail.com>
2026-06-13 13:00:44 +0800

    feat; initiate css n js config

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+import Alpine from "alpinejs";
+import flatpickr from "flatpickr";
+import TomSelect from "tom-select";
+import dayjs from "dayjs";
+
+window.Alpine = Alpine;
+window.flatpickr = flatpickr;
+window.TomSelect = TomSelect;
+window.dayjs = dayjs;
+
+Alpine.start();
