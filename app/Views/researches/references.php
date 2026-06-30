4867343dc3cadca275c7bb3b54b9bdc99f710c56
syahfrinorezky <syahfrinorezky@gmail.com>
2026-06-30 09:09:51 +0800

    feat: update research references view

--- app/Views/researches/references.php
+++ app/Views/researches/references.php
@@ -111,7 +111,7 @@
                         <option value="">-- Pilih Periode --</option>
                         <?php foreach ($periods as $p) : ?>
                             <option value="<?= $p['id'] ?>" <?= $selectedPeriod == $p['id'] ? 'selected' : '' ?>>
-                                <?= esc($p['nama_periode']) ?> (<?= esc($p['tahun_akademik']) ?>)
+                                <?= format_periode($p['nama_periode'], $p['tahun_akademik']) ?>
                             </option>
                         <?php endforeach; ?>
                     </select>
